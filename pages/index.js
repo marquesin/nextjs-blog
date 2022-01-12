9ec659c4cd6cd41001b8c8122ad548164924b06b
marquesin <marquesinro@gmail.com>
2022-01-12 15:06:09 -0300

    Actualizacion de contenido

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Soy Rocio y estoy haciendo esta prueba con nextJs]</p>
+        <p>[Soy Rocio y estoy usando NextJs para crear este blog tan mono]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
